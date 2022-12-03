28efb3bf4ec23284f54b8d2ad530515b1e62c202
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-12-03 12:07:02 +0500

    quiz api create

--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+# from rest_framework import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication,BasicAuthentication
+
+from .models import Question,Quiz,Option,Result,Result_detail,User
+from .serializers import Questionserializer,Quizserializer,Optionserializer
+
+class Quiz(APIView):
+    permission_classes = [IsAuthenticated]
+    # quiz=Quiz.objects.all()
+    # serializer=Quizserializer(quiz,many=True)
+    def post(self, request: Request):
+        """ 
+        Create a new quiz
+        """
+        serializer=Quizserializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
