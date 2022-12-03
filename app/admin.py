2667d6ba77045a7c7ee811ee7a20866ff70ba57e
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2022-12-03 10:21:30 +0500

    models regester

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question,Quiz,User,Result,Result_detail,Option
+
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(Result)
+admin.site.register(Result_detail)
+admin.site.register(Option)
